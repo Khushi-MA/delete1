e0809193bbcf4a4f840818d5dc68c1fb3187c6a5
Khushi-MA <khushi8599@yahoo.com>
2026-06-22 22:19:32 +0530

    footer down achieved

--- src/App.jsx
+++ src/App.jsx
@@ -17,20 +17,23 @@ import Work from "./pages/work";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <div className="pageLayout">
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/awards" element={<Awards />} />
-        <Route path="/books" element={<Books />} />
-        <Route path="/family" element={<Family />} />
-
-      </Routes>
+        <main className="contentArea">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/awards" element={<Awards />} />
+            <Route path="/books" element={<Books />} />
+            <Route path="/family" element={<Family />} />
+          </Routes>
+        </main>
+      </div>
 
       <Footer />
     </BrowserRouter>
